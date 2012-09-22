d2310c46ab537b80e87f27ba4105994c0a4fd691
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2012-09-22 14:26:05 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# Facebook Open Graph
+gem 'omniauth'
+gem 'omniauth-facebook'
+gem 'koala'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vocalgrader::Application.config.secret_token = 'b4c147b655da5013d67a45dbf711241e173372f29ec9fbc11ee3195645f09af89fae9ae667b54f9e99e01f445dc4088bdc39d87236bfd6b46cf4cf07c1c8ac1f'
