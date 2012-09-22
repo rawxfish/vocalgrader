822ddc189eb8b701cf5e8aa25c781a08aa51d5ee
Heinrich Jiang <heinrich.jiang@gmail.com>
2012-09-22 17:12:02 -0400

    add attachment files

--- app/models/actual_wave.rb
+++ app/models/actual_wave.rb
@@ -2,4 +2,10 @@ class ActualWave < ActiveRecord::Base
   attr_accessible :user_id
 
   belongs_to :trial
+
+  attr_accessible :file
+
+  has_attached_file :file, :path => "uploads/#{:id}-actual.:extension"
+  validates_attrachment_presence :file, :content_type => 'audio/x-wav'
+
 end
--- app/models/expected_wave.rb
+++ app/models/expected_wave.rb
@@ -1,5 +1,9 @@
 class ExpectedWave < ActiveRecord::Base
   attr_accessible :user_id
-
   belongs_to :trial
+
+  attr_accessible :file
+  has_attached_file :file, :path => "uploads/#{id}-expected.:extension"
+  validates_attachment_presence :file, :content_type => 'audio/x-wav'
+
 end
