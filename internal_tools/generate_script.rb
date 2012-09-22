cb61be224d99b88da585af6be614a2ce6616e0b8
Heinrich Jiang <heinrich.jiang@gmail.com>
2012-09-22 15:29:16 -0400

    edit scripts

--- internal_tools/generate_script.rb
+++ internal_tools/generate_script.rb
@@ -5,8 +5,10 @@ id = ARGV.first
 file = File.new("internal_tools/script_#{id}.sh". 'w')
 
 file.puts("#!/bin/bash\n")
-file.puts("aubionotes -i uploads/#{id}.wav -v > trial_data/#{id}-notes\n")
-file.puts("aubiotrack -i uploads/#{id}.wav -v > trial_data/#{id}-track\n")
+file.puts("aubionotes -i uploads/#{id}-expected.wav -v > trial_data/#{id}-expected-notes\n")
+file.puts("aubiotrack -i uploads/#{id}-expected.wav -v > trial_data/#{id}-expected-track\n")
+file.puts("aubionotes -i uploads/#{id}-actual.wav -v > trial_data/#{id}-actual-notes\n")
+file.puts("aubiotrack -i uploads/#{id}-actual.wav -v > trial_data/#{id}-actual-track\n")
 file.close
 
 system("chmod +x internal_tools/script_#{id}.sh")
