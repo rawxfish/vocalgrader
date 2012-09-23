87097781345e1dafe016ff01bdab71b241510890
Heinrich Jiang <heinrich.jiang@gmail.com>
2012-09-23 07:44:50 -0400

    caoooo

--- app/controllers/trials_controller.rb
+++ app/controllers/trials_controller.rb
@@ -24,7 +24,27 @@ class TrialsController < ApplicationController
   end 
   
   def judge
+    @trial = Trial.find(params[:trial_id])
+    @attempt_id = params[:actual_id]
+
+    @trial.generate_data(@attempt_id)
+    file = File.open("output_data/#{@trial.expected_wave.id}-#{@attempt_id}", "r")
+    @grades = Array.new
+    @freq1 = Array.new
+    @freq2 = Array.new
+    @max_freq = 0;
+    i = 0
+    file.each_line do |line|
+      if i < 4
+        @grades[i] = line.to_f
+      else
+        stuff = line.split(" ")
+        @freq1[i - 4] = stuff[0].to_f
+        @freq2[i - 4] = stuff[1].to_f
+      end
+      i = i + 1
+    end
+    @max_freq = i - 4
     
   end
-
 end
